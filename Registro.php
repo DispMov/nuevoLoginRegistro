674a70c42536ac6ef83059ecffec7b3bf7534544
karlyyhdz <isckarla_hdz@hotmail.com>
2016-05-01 02:51:15 -0500

    Update Registro.php

--- Registro.php
+++ Registro.php
@@ -1,5 +1,5 @@
 <?php
-    $con = mysqli_connect("	mysql13.000webhost.com", "a4243694_karlyy", "12karlyyhdz", "a4243694_ejemplo");
+    $con = mysqli_connect("mysql13.000webhost.com", "a4243694_karlyy", "12karlyyhdz", "a4243694_ejemplo");
     
     $nombre = $_POST["nombre"];
     $edad = $_POST["edad"];
