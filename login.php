4f3367b7cab141e1d9317c680b30ee278a0f451c
karlyyhdz <isckarla_hdz@hotmail.com>
2016-05-01 02:56:14 -0500

    Update login.php

--- login.php
+++ login.php
@@ -2,24 +2,24 @@
     $con = mysqli_connect("mysql13.000webhost.com", "a4243694_karlyy", "12karlyyhdz", "a4243694_ejemplo");
     
     $usernombre = $_POST["usernombre"];
-    $contrasena = $_POST["contraseña"];
+    $Password = $_POST["Password"];
     
-    $statement = mysqli_prepare($con, "SELECT * FROM usuario WHERE usernombre = ? AND contraseña = ?");
-    mysqli_stmt_bind_param($statement, "ss", $usernombre, $contraseña);
+    $statement = mysqli_prepare($con, "SELECT * FROM usuario WHERE usernombre = ? AND Password = ?");
+    mysqli_stmt_bind_param($statement, "ss", $usernombre, $Password);
     mysqli_stmt_execute($statement);
     
     mysqli_stmt_store_result($statement);
-    mysqli_stmt_bind_result($statement, $usuarioid, $nombre, $edad, $usernombre, $contraseña);
+    mysqli_stmt_bind_result($statement, $usuarioid, $nombre, $edad, $usernombre, $Password);
     
     $response = array();
     $response["success"] = false;  
     
     while(mysqli_stmt_fetch($statement)){
         $response["success"] = true;  
-        $response["name"] = $nombre;
-        $response["age"] = $edad;
-        $response["username"] = $usernombre;
-        $response["password"] = $contraseña;
+        $response["nombre"] = $nombre;
+        $response["edad"] = $edad;
+        $response["usernombre"] = $usernombre;
+        $response["password"] = $Password;
     }
     
     echo json_encode($response);
