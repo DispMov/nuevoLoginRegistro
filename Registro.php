ef8b3f9daff40a0900c42a3a7f516efa2cca5513
karlyyhdz <isckarla_hdz@hotmail.com>
2016-04-30 23:41:25 -0500

    Update Registro.php

--- Registro.php
+++ Registro.php
@@ -2,11 +2,11 @@
     $con = mysqli_connect("	mysql13.000webhost.com", "a4243694_karlyy", "12karlyyhdz", "a4243694_ejemplo");
     
     $nombre = $_POST["nombre"];
+    $edad = $_POST["edad"];
     $usernombre = $_POST["usernombre"];
     $contraseña = $_POST["contraseña"];
-    $edad = $_POST["edad"];
-    $statement = mysqli_prepare($con, "INSERT INTO usuario (nombre, usernombre, contraseña,edad) VALUES (?, ?, ?, ?)");
-    mysqli_stmt_bind_param($statement, "siss", $nombre, $usernombre, $contraseña, $edad);
+    $statement = mysqli_prepare($con, "INSERT INTO usuario (nombre,edad, usernombre, contraseña) VALUES (?, ?, ?, ?)");
+    mysqli_stmt_bind_param($statement, "siss", $nombre, $edad, $usernombre, $contraseña);
     mysqli_stmt_execute($statement);
     
     $response = array();
